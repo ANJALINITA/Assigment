5ccbabf5c02843b92b4a63beea2fffe93ae53b53
ANJALINITA <anjalisingh22122003@gmail.com>
2024-09-20 02:22:20 +0530

    Navbar implemented

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState} from 'react';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
+  const [group, setGroup] = useState('status');
+  const [order, setOrder] = useState('priority');
+  const handleGroupChange = (groupSelected) => {
+    setGroup(groupSelected);
+    localStorage.setItem("selectedGroup", groupSelected);
+  }
+  const handleOrderChange = (orderSelected) => {
+    setOrder(orderSelected);
+    localStorage.setItem("selectedOrder", orderSelected);
+  }
   return (
-   <div>
-    anjali singh
-   </div>
+   <div>Project setup</div>
+    <div className="App scroll-container">
+      <Navbar 
+        group={group} 
+        order={order} 
+        onGroupChange={handleGroupChange} 
+        onOrderChange={handleOrderChange} 
+      />
+    </div>
   );
 }
-
-export default App;
